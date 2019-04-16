d29cdbae2dcabbee9a0194a78b6851fc0f229466
Carter Rabasa <carter.rabasa@gmail.com>
2019-04-16 13:37:04 -0700

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,10 +1,3 @@
-// Add simple, fast, scalable persistence: https://docs.begin.com/en/data/begin-data/
-// let data = require('@begin/data')
-
-// Add secure sessions, middleware, and more: https://docs.begin.com/en/functions/http/
-// let begin = require('@architect/functions')
-
-// TODO: modify the body object!
 let body = `
 <!doctype html>
 <html lang=en>
@@ -12,17 +5,19 @@ let body = `
     <meta charset=utf-8>
     <title>Hi!</title>
     <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
   </head>
   <body>
 
     <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+      CascadiaJS 2019
     </h1>
 
     <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
+      When: November 7 - 8, 2019
+    </p>
+
+    <p class="center-text">
+      Where: <a href="https://townhallseattle.org/">Town Hall</a> (Seattle, WA, USA)
     </p>
 
   </body>
@@ -36,35 +31,3 @@ exports.handler = async function http(req) {
     body
   }
 }
-
-// Example responses
-
-/* Forward requester to a new path
-exports.handler = async function http(req) {
-  return {
-    status: 302,
-    location: '/about',
-  }
-}
-*/
-
-/* Respond with successful resource creation, CORS enabled
-exports.handler = async function http(req) {
-  return {
-    status: 201,
-    type: 'application/json',
-    body: JSON.stringify({ok: true}),
-    cors: true,
-  }
-}
-*/
-
-/* Deliver client-side JS
-exports.handler = async function http(req) {
-  return {
-    type: 'text/javascript',
-    body: 'console.log("Hello world!")',
-  }
-}
-*/
-
