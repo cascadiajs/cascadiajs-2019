f5e21f09b8d52c4e81305dc34b453037c443d538
Carter Rabasa <carter.rabasa@gmail.com>
2019-04-16 17:27:09 -0700

    Added sparkle

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -25,7 +25,7 @@ let body = `
     </p>
 
     <p class="center-text">
-      <i>This website is powered by <a href="https://begin.com">Begin</a></i>
+      <i>✨This website is powered by <a href="https://begin.com">Begin</a></i>
     </p>
 
   </body>
