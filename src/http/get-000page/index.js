6855bc4f706ce56d4eb765351895af9989d7b5ec
Carter Rabasa <carter.rabasa@gmail.com>
2019-05-14 14:24:47 -0700

    _ci: removing src/http/get-000page/index.js for route

--- src/http/get-000page/index.js
+++
@@ -1,9 +0,0 @@
-let arc = require("@architect/functions")
-let render = require("@architect/shared/render")
-
-function route(req, res) {
-	let { page } = req.params
-	res(render(page))
-}
-
-exports.handler = arc.http(route)
