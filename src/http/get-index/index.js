e13994921da25b9e46d1a84036e130241d840080
Carter Rabasa <carter@rabasa.com>
2019-06-30 09:54:43 -0700

    switched to Regular round of tickets + React updates

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -49,7 +49,7 @@ let content = `
       NOTE: If you are paying yourself, work at a non-profit or a company with less than 10 people, feel free to use the promo code INDIE for $200 off.
     </p>
     <p>
-      <a class="cta" href="https://fizbuz.com/signup?placeID=PLoz9mrq5pvw8xvvnomxq71kjlngx406&forwardUrl=https%3A%2F%2Fti.to%2Fevent-loop%2Fcascadiajs-2019%2Fwith%2F4-wqkvqvp-a%2Cluwnj6-r4zi%2Cmenxgsfya6k%2Cciqxti6a5yu%2Cqzjnfesyonm%2Cgxtdhvhs2s8%2Ccip51mg0glk%2C5atc3qhk-60">Buy a Ticket</a>
+      <a class="cta" href="https://fizbuz.com/signup?placeID=PLoz9mrq5pvw8xvvnomxq71kjlngx406&forwardUrl=https%3A%2F%2Fti.to%2Fevent-loop%2Fcascadiajs-2019%2Fwith%2Fnfb9f7-vxei%2C2jsnjwraibu%2Cdarlfcbexuy%2Coxxgtirrr5o%2Cgxtdhvhs2s8%2Cqzjnfesyonm%2Ccip51mg0glk%2C5atc3qhk-60">Buy a Ticket</a>
     </p>
   </section>
   
