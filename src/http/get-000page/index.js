99e57805e1734326df2d0750e6ea2b62fe700e94
Ryan Block <rblock@gmail.com>
2019-09-03 08:53:17 -0700

    Remove get /:page (to be handled by get /)

--- src/http/get-000page/index.js
+++
@@ -1,8 +0,0 @@
-let arc = require("@architect/functions")
-let handler = require("@architect/shared/handler")
-
-function route(req, res) {
-	handler(req, res)
-}
-
-exports.handler = arc.http(route)
