05b94fbf3a2145ab4934cb285ec79e7f4e972132
jimcal <jimcal.liu@gmail.com>
2019-09-10 00:02:27 -0700

    update to adhere to the formatting rule

--- src/views/index/events.js
+++ src/views/index/events.js
@@ -1,29 +1,29 @@
-let eventsData = require("./events.json");
+let eventsData = require('./events.json')
 
 module.exports = function Events() {
-  let events = [];
+  let events = []
   eventsData.forEach(event => {
-    let { date, eventName, link } = event;
-    let now = new Date();
+    let { date, eventName, link } = event
+    let now = new Date()
     // Only show events from this or next month
-    let monthDelta = Number(date.split("/")[0]) - now.getMonth() - 1;
+    let monthDelta = Number(date.split('/')[0]) - now.getMonth() - 1
     // Only show events for running 30 days range
-    let dayDelta = 30 * monthDelta + Number(date.split("/")[1]) - now.getDate();
+    let dayDelta = 30 * monthDelta + Number(date.split('/')[1]) - now.getDate()
 
-    if (monthDelta < 0 || monthDelta > 1) return;
-    if (dayDelta > 30) return;
+    if (monthDelta < 0 || monthDelta > 1) return
+    if (dayDelta > 30) return
 
-    link = link ? `<a href="${link}">` : "";
+    link = link ? `<a href="${link}">` : ''
     let item = `
 <tr>
   <td>${date}</td>
   <td>
-    ${link}${eventName}${link ? "</a>" : ""}
+    ${link}${eventName}${link ? '</a>' : ''}
   </td>
 </tr>
-    `;
-    events.push(item);
-  });
+    `
+    events.push(item)
+  })
 
-  return events.join("\n");
-};
+  return events.join('\n')
+}
