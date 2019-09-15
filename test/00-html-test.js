3f12119c31c891ee3b3a0549578768f045b57f69
Ryan Block <rblock@gmail.com>
2019-09-14 19:57:25 -0700

    Update html test

--- test/00-html-test.js
+++ test/00-html-test.js
@@ -1,6 +1,7 @@
 let test = require('tape')
 let tiny = require('tiny-json-http')
 let sandbox = require('@architect/architect').sandbox
+let url = 'http://localhost:6666'
 
 test('env', t => {
   t.plan(1)
@@ -11,20 +12,18 @@ let end // saves a reference to be used later to shut down the sandbox
 test('sandbox.start', async t => {
   t.plan(1)
   end = await sandbox.start()
-  t.ok(true, 'opened')
+  t.ok(end, 'sandbox started')
 })
 
 // callback style
 test('get /', t => {
   t.plan(1)
-  tiny.get({
-    url: 'http://localhost:6666'
-  },
+  tiny.get({url},
   function win (err, result) {
     if (err) {
       t.fail(err, err)
     } else {
-      t.ok(true, 'got result', console.log(result.toString().substring(50) + '...'))
+      t.ok(true, 'got result', console.log(result.body.toString().substring(0,50) + '...'))
     }
   })
 })
@@ -32,29 +31,28 @@ test('get /', t => {
 // promise style
 test('get /', t => {
   t.plan(1)
-  tiny.get({
-    url: 'http://localhost:6666'
-  }).then(function win (result) {
-    t.ok(true, 'got result', console.log(result.toString().substring(50) + '...'))
-  }).catch(function fail (err) {
-    t.fail(err, err)
-  })
+  tiny.get({url})
+    .then(function win (result) {
+      t.ok(true, 'got result', console.log(result.body.toString().substring(0,50) + '...'))
+    })
+    .catch(function fail (err) {
+      t.fail(err, err)
+    })
 })
 
 // async/await style
 test('get /', async t => {
   t.plan(1)
   try {
-    var url = 'http://localhost:6666'
     var result = await tiny.get({url})
-    t.ok(true, 'got result',console.log(result.toString().substring(50) + '...'))
+    t.ok(true, 'got result',console.log(result.body.toString().substring(0,50) + '...'))
   } catch (e) {
     t.fail(e, e, console.log(e))
   }
 })
 
-test('shut down the sandbox', t=> {
+test('sandbox.close', t=> {
   t.plan(1)
   end()
-  t.ok(true, 'shutdown successfully')
+  t.pass('sandbox shut down')
 })
