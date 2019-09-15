c53963b37fc62f609e3759ae2986a2b92055851e
Ryan Block <rblock@gmail.com>
2019-09-15 14:38:17 -0700

    Uncommenting attendee data

--- src/views/layout/index.js
+++ src/views/layout/index.js
@@ -1,10 +1,10 @@
 let mainLayout = require('./layout')
-// let getAttendeeData = require('@architect/shared/get-attendee-data')
-// let Attendees = require('./attendees')
+let getAttendeeData = require('@architect/shared/get-attendee-data')
+let Attendees = require('./attendees')
 
 module.exports = async function Layout (props) {
   let title = props.title || 'Nov 7-8, 2019 | Seattle, WA, USA'
-  // let attendeeData = await getAttendeeData()
-  // let attendees = Attendees({attendeeData})
-  return mainLayout({...props, title})
+  let attendeeData = await getAttendeeData()
+  let attendees = Attendees({attendeeData})
+  return mainLayout({...props, title, attendees})
 }
