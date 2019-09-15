68d0f3885cf03941f6ce9e4b8d281cf8cfed43d5
Jim Liu <jimcal@users.noreply.github.com>
2019-09-15 11:32:43 -0700

    Add September events (#6)
    * Add September events and beyond
    
    events.json: adding events range from September to December 2019.
    events.js: adding logic to show rolling 30 days.
    
    * update to adhere to the formatting rule

--- src/views/index/events.js
+++ src/views/index/events.js
@@ -1,12 +1,17 @@
 let eventsData = require('./events.json')
 
-module.exports = function Events () {
+module.exports = function Events() {
   let events = []
   eventsData.forEach(event => {
-    let {date, eventName, link} = event
-    // Only show events from this or last month
+    let { date, eventName, link } = event
     let now = new Date()
-    if (now.getMonth() - date.split('/')[0] >= 1) return
+    // Only show events from this or next month
+    let monthDelta = Number(date.split('/')[0]) - now.getMonth() - 1
+    // Only show events for running 30 days range
+    let dayDelta = 30 * monthDelta + Number(date.split('/')[1]) - now.getDate()
+
+    if (monthDelta < 0 || monthDelta > 1) return
+    if (dayDelta > 30) return
 
     link = link ? `<a href="${link}">` : ''
     let item = `
