dc85220b9eb59047641b44ec98b0d54208b99e5f
Carter Rabasa <carter@rabasa.com>
2019-09-16 11:44:41 -0700

    removed console.logging

--- src/shared/get-attendee-data.js
+++ src/shared/get-attendee-data.js
@@ -4,7 +4,7 @@ const tiny = require('tiny-json-http')
 const fetchFromApi = async () => {
   let url = 'https://fizbuz.com/graphql'
   let data = {query: '{accountsByPlaceID(placeID:"PLoz9mrq5pvw8xvvnomxq71kjlngx406"){name,nickname,image}}'}
-  console.log('calling API')
+  //console.log('calling API')
   let attendeeData = await tiny.post({url, data})
   // should probably cache this data some kind of smart way (5 minutes or so)
   return attendeeData.body.data.accountsByPlaceID
@@ -17,15 +17,15 @@ module.exports = async function getSpeakerData () {
   let key = 'cascadiajs2019'
 
   try {
-    console.log('Reading attendees from Begin Data')
+    //console.log('Reading attendees from Begin Data')
     attendees = await data.get({table, key})
     attendeeData = attendees && attendees.attendeeData
     if (!attendeeData) {
-      console.log('attendees not in DB, fetching from API')
+      //console.log('attendees not in DB, fetching from API')
       attendeeData = await fetchFromApi()
       let epoch = Date.now() / 1000
       let ttl = epoch + (60*5) // 5 minutes
-      console.log(`Writing attendees to Begin Data and caching for 5 minutes`)
+      //console.log(`Writing attendees to Begin Data and caching for 5 minutes`)
       attendees = await data.set({table, key, ttl, attendeeData})
       attendeeData = attendees.attendeeData
     }
